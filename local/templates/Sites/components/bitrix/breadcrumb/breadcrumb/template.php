9f14a84cf0c5db63a03df5cd0db20a35c2dc1f31
Frem <Shchale@yandex.ru>
2019-04-07 13:23:39 +0300

    Хлебные крошки

---
+++ local/templates/Sites/components/bitrix/breadcrumb/breadcrumb/template.php
@@ -0,0 +1,36 @@
+<?php
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+/**
+ * @global CMain $APPLICATION
+ */
+
+global $APPLICATION;
+
+//delayed function must return a string
+if(empty($arResult))
+	return "";
+
+$strReturn = '';
+
+
+$strReturn .= '<ol class="breadcrumb">';
+
+$itemSize = count($arResult);
+for($index = 0; $index < $itemSize; $index++)
+{
+	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
+
+	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
+	{
+		$strReturn .= '<li><a href="'.$arResult[$index]["LINK"].'">'.$title.'</a></li>';
+	}
+	else
+	{
+		$strReturn .='<li class="active">'.$title.'</li>';
+	}
+}
+
+$strReturn .= '</ol>';
+
+return $strReturn;
