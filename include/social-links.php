d8f7e39cfb0903d13c72d6e95eb235e4bf4f5971
Frem <Shchale@yandex.ru>
2019-04-16 20:32:42 +0300

    Файлы для редоктируемых областей

---
+++ include/social-links.php
@@ -0,0 +1,12 @@
+<ul class="list-inline social-links">
+    <li><a href="https://vk.com/intervolga"><i class="fa fa-vk"></i></a></li>
+    <li><a href="https://www.facebook.com/intervolga34"><i class="fa fa-facebook"></i></a></li>
+    <li><a href="https://twitter.com/intervolga_ru"><i class="fa fa-twitter"></i></a></li>
+    <li><a href="mailto:info@intervolga.ru"><i class="fa fa-envelope"></i></a></li>
+    <li><a href="https://www.youtube.com/user/wwwintervolgaru"><i class="fa fa-youtube"></i></a>
+    </li>
+    <li><a href="https://instagram.com/intervolga_ru/"><i class="fa fa-instagram"></i></a></li>
+    <li>
+        <div id="bx-composite-banner"></div>
+    </li>
+</ul>
