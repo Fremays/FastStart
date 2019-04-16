d3b0b2648f5d799caf693ec56e80e07599ddd346
Frem <Shchale@yandex.ru>
2019-04-16 20:30:31 +0300

    Размещен компонент поиск

--- search/index.php
+++ search/index.php
@@ -1,8 +1,37 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Поиск");
-?>
-
-Text here....
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:search.page", 
+	"search", 
+	array(
+		"COMPONENT_TEMPLATE" => "search",
+		"RESTART" => "Y",
+		"NO_WORD_LOGIC" => "N",
+		"CHECK_DATES" => "N",
+		"USE_TITLE_RANK" => "N",
+		"DEFAULT_SORT" => "rank",
+		"FILTER_NAME" => "",
+		"arrFILTER" => array(
+			0 => "no",
+		),
+		"SHOW_WHERE" => "N",
+		"SHOW_WHEN" => "N",
+		"PAGE_RESULT_COUNT" => "10",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "3600",
+		"USE_LANGUAGE_GUESS" => "N",
+		"USE_SUGGEST" => "N",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"PAGER_TITLE" => "Результаты поиска",
+		"PAGER_SHOW_ALWAYS" => "Y",
+		"PAGER_TEMPLATE" => ""
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
